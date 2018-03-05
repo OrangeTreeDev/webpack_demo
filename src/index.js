7f1ad7ff9317eb525f13bba776f9e1826249b507
sunlixiang <sunlixiang@csvw.com>
2018-03-05 16:10:33 +0800

    asset manage chapter

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import _ from 'lodash';
+import './main.css';
 
 function component() {
     var element = document.createElement('div');
-    
+    element.className = 'app';
     // 通过import引入外部依赖
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     return element;
---
+++ webpack.config.js
@@ -0,0 +1,56 @@
+var path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'images/[name].[hash:7].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'fonts/[name].[hash:7].[ext]'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 10000,
+              name: 'medias/[name].[hash:7].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  }
+};
